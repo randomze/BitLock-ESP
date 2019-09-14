e523cd2fa6d18f831a3e048c05206aca115cdbe5
José Pedro <zemiguel.apedro@gmail.com>
2019-09-14 22:59:55 +0100

    agora ate crasha yey

--- master_esp/master_esp.ino
+++ master_esp/master_esp.ino
@@ -1,3 +1,6 @@
+#include <map>
+#include <queue>
+
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 #include <WiFiUdp.h>
@@ -11,10 +14,13 @@ const uint32_t gateway = local_IP;
 const uint32_t subnet = IP(255, 255, 255, 0);
 
 const uint16_t masterPort = 2004;
-const uint16_t slavePort = 2016;
+const uint16_t UDPSlavePort = 2016;
+const uint16_t TCPSlavePort = 2019;
+
+bool waitingForRegister;
 
 ESP8266WebServer serverGateway(80);
-ESP8266WebServer *serverClient;
+ESP8266WebServer *serverMaster;
 WiFiUDP udp;
 HTTPClient client;
 
@@ -24,13 +30,11 @@ String password;
 
 IPAddress broadcastAddress;
 
-//clientserver = 0 means its in soft ap clientserver in order to find a network to connect to. clientserver = 1 means its in client clientserver and connected to a wifi network
-int serverclient;
+int gatewayOrMaster;
 
 int EEPROMaddress;
 
-char packetBuffer[255]; //buffer to hold incoming packet
-char ReplyBuffer[] = "acknowledged";       // a string to send back
+std::map<String, WiFiClient> slaveMap;
 
 //SERVER CODE
 
@@ -93,209 +97,51 @@ void handleSubmit()
     ssid = serverGateway.arg("network");
     password = serverGateway.arg("password");
 
-    setupClient();
+    setupMaster();
   }
-}
-
-void setupSoftAP(void)
-{
-  WiFi.softAPConfig(local_IP, gateway, subnet);
-  WiFi.softAP("Bit_Lock");
-
-  serverGateway.on("/", handleServerRoot);
-
-  serverGateway.begin();
 }
 
 //CLIENT CODE
-
-void setupClient() {
-  WiFi.begin(ssid, password);
-
-  int result;
-  int tries = 1;
-  while((result = WiFi.status()) != WL_CONNECTED && tries < 20) {
-    tries++;
-    delay(500);
-  }
-  if (result == WL_CONNECTED) {
-    if (serverclient == 0) {
-      serverGateway.send(200, "text/plain", "Sucessfully connected");
-      serverGateway.stop();
-      WiFi.softAPdisconnect();
-    }
-
-    WiFi.softAPConfig(local_IP, gateway, subnet);
-    WiFi.softAP("BitLockMesh", "bitlockmesh", 1, 1);
-
-    Serial.println("does this happen");
-
-    serverClient = new ESP8266WebServer(WiFi.localIP(), 80);
-    if (EEPROMaddress == 0) {
-      int i;
-      String temp = "wl=";
-      EEPROM.begin(256);
-      for (i = 0; i < 3; i++) {
-        EEPROM.write(EEPROMaddress + i, temp[i]);
-        delay(200);
-      }
-      EEPROMaddress += i;
-
-      for (i = 0; i < ssid.length(); i++) {
-        EEPROM.write(EEPROMaddress + i, ssid[i]);
-        delay(200);
-      }
-      EEPROM.write(EEPROMaddress + i, '\0');
-
-      EEPROMaddress += i + 1;
-      for (i = 0; i < password.length(); i++) {
-        EEPROM.write(EEPROMaddress + i, password[i]);
-        delay(200);
-      }
-      EEPROM.write(EEPROMaddress + i, '\0');
-      delay(200);
-
-      EEPROM.commit();
-
-      EEPROMaddress += i + 1;
-    }
-
-    broadcastAddress = (uint32_t)WiFi.softAPIP() | ~subnet;
-
-    serverClient->on("/", handleClientRoot);
-    serverClient->on("/devices/", handleClientRegisterDevice);
-    serverClient->on("/reset", handleReset);
-
-    serverClient->begin();
-
-    //NEVER FORGET, EURO 2004
-    udp.begin(2004);
-    serverclient = 1;
-  } else {
-    serverGateway.send(500, "text/plain", "Failed to connect");
-    serverclient = 0;
-  }
-}
-
-void handleClientRoot() {
-  if (serverClient->method() != HTTP_POST) {
-    if (serverClient->method() == HTTP_GET && master_unique != "") {
-      serverClient->send(200, "text/plain", master_unique);
-    } else {
-      serverClient->send(200, "text/plain", "ITS A ME, BITLOCK!");
-    }
-  }
-  else if (master_unique = "")
-  {
-    master_unique = serverClient->arg("plain");
-
-    EEPROM.begin(256);
-
-    int i = 0;
-    master_unique = String("id=") + master_unique;
-    for (i; i < master_unique.length(); i++) {
-      EEPROM.write(EEPROMaddress + i, master_unique[i]);
-      delay(200);
-    }
-
-    EEPROM.write(EEPROMaddress + i, '\0');
-    delay(200);
-
-    EEPROM.commit();
-
-    master_unique = master_unique.substring(4);
-
-    serverClient->send(200, "text/plain", "OK!");
-  } else {
-    serverClient->send(200, "text/plain", "Already registered, please stop");
-  }
-}
-
-void handleClientRegisterDevice() {
-  char response[255];
-  char message[255] = "\0";
-
-  if (serverClient->method() == HTTP_GET) {
-    while (!sendPacket((uint8_t *)strcat(message, "REG_WAIT?"), strlen(message) + 1)) Serial.println("trying to send packet");
-
-    delay(1000);
-    int packetSize = udp.parsePacket();
-    if (packetSize) {
-      int len = udp.read(response, 255);
-      if (len > 0) {
-        response[len] = 0;
-      }
-    }
-
-    Serial.println(response);
-
-    if (strcmp(response, "NO") == 0) {
-      serverClient->send(200, "text/plain", "No device to register");
-    } else if (strcmp(response, "YES") == 0) {
-      serverClient->send(200, "text/plain", "Device waiting to be registered");
-    } else {
-      serverClient->send(200, "text/plain", "Unable to get a proper response");
-    }
-
-  } else if (serverClient->method() == HTTP_POST) {
-    String device_id = serverClient->arg("plain");
-
-    while (!sendPacket((uint8_t* )strcat(strcat(message, "REGISTER AS "), device_id.c_str()), strlen(message))) Serial.println("trying to send packet");
-
-    int packetSize = udp.parsePacket();
-    if (packetSize) {
-      int len = udp.read(response, 255);
-      if (len > 0) {
-        response[len] = 0;
-      }
-    }
-    if (strcmp(response, "DONE") == 0) {
-      serverClient->send(200, "text/plain", "DONE");
-    } else {
-      serverClient->send(200, "text/plain", "Unable to register");
-    }
-  } else if(serverClient->method() == HTTP_DELETE) {
-    String device_id = serverClient->arg("plain");
-
-    while (!sendPacket((uint8_t* )strcat(strcat(message, "DELETE "), device_id.c_str()), strlen(message))) Serial.println("trying to send packet");    
-  } else {
-    serverClient->send(405, "text/plain", "Method not allowed");
-  }
-}
 
 void checkForMessage() {
   const char* host = "https://bitlock-api.herokuapp.com/devices/waiting/";
 
   String thumbprint = "08:3B:71:72:02:43:6E:CA:ED:42:86:93:BA:7E:DF:81:C4:BC:62:30";
-  
+
   if (client.begin(String(host) + master_unique, thumbprint)) {
     int statusCode = client.GET();
     String response;
     if (statusCode > 0) {
       response = client.getString();
     }
-    if(response.startsWith("OPEN")) {
-      while (!sendPacket((uint8_t *)response.c_str(), response.length() + 1)) Serial.println("trying to send packet");  
+    if (response.startsWith("OPEN")) {
+      String slaveId = response.substring(6);
+      slaveId.trim();
+      if(slaveMap.find(slaveId) != slaveMap.end()) {
+        WiFiClient client  = slaveMap[slaveId];
+        if(client.connected()) {
+          Serial.println("Connected to " + slaveId);
+          client.print("OPEN\n");
+          client.flush();
+        } else {
+          Serial.println("Couldnt connect to " + slaveId);
+        }
+      }
     }
   }
-}
-
-bool sendPacket(const uint8_t* buf, uint8_t bufSize) {
-  udp.beginPacket(broadcastAddress, slavePort);
-  udp.write(buf, bufSize);
-  return (udp.endPacket() == 1);
 }
 
 void setup(void)
 {
   Serial.begin(9600);
-  WiFi.mode(WIFI_AP_STA);
   WiFi.setAutoConnect(false);
+  WiFi.mode(WIFI_AP_STA);
   char buffer[65];
 
   master_unique = "";
   int i;
   EEPROMaddress = 0;
+  waitingForRegister = 0;
 
   EEPROM.begin(256);
 
@@ -329,14 +175,14 @@ void setup(void)
       EEPROMaddress += i + 1;
     }
 
-    serverclient = 1;
-    setupClient();
-    if(serverclient == 0) {
-      setupSoftAP();
+    gatewayOrMaster = 1;
+    setupMaster();
+    if (gatewayOrMaster == 0) {
+      setupConnectGateway();
     }
   } else {
-    serverclient = 0;
-    setupSoftAP();
+    gatewayOrMaster = 0;
+    setupConnectGateway();
   }
 
 }
@@ -350,24 +196,282 @@ void handleReset() {
 
 void loop(void)
 {
-  if (serverclient == 1) {
-    delay(1000);
-    serverClient->handleClient();
-    int packetSize = udp.parsePacket();
-    if (packetSize) {
-      IPAddress remoteIp = udp.remoteIP();
-
-      int len = udp.read(packetBuffer, 255);
-      if (len > 0) {
-        packetBuffer[len] = 0;
-      }
-      
-      udp.beginPacket(udp.remoteIP(), udp.remotePort());
-      udp.write(ReplyBuffer);
-      udp.endPacket();
-    }
+  if (gatewayOrMaster == 1) {
+    delay(500);
+    findSlavesOnNetwork();
+    serverMaster->handleClient();
+    receivePacket();
     checkForMessage();
   } else {
     serverGateway.handleClient();
   }
+}
+
+
+void findSlavesOnNetwork()
+{
+  char message[] = "Anyone there?";
+  sendPacket((uint8_t *) message, strlen(message) + 1);
+
+  int i = 0;
+  while(!udp.parsePacket()) {
+    i++;
+    delay(400);
+    if(i > 5)
+      break;
+  }
+
+  String packet = receivePacket();
+
+  Serial.println(packet);
+
+  if(packet.equals("Yes")) {
+    Serial.println("FOUND SLAVE");
+    WiFiClient client;
+    if(client.connect(udp.remoteIP(), TCPSlavePort)) {
+      client.print("REG_WAIT?\n");
+      client.flush();
+      Serial.println("SUCESSFULLY CONNECTED TO SLAVE OVER TCP");
+      if(client.available()) {
+        String response = client.readStringUntil('\n');
+        Serial.println("response: " + response);
+        if(response.equals("YES")) {
+          Serial.println("response==YES");
+          slaveMap[""] = client;
+          waitingForRegister = true;
+        } else {
+          Serial.println("response!=YES");
+          slaveMap[response] = client;
+        }
+      }
+    } else {
+      Serial.println("tcp failed");
+    }
+  }
+}
+
+//
+//
+//   UDP helper functions
+//
+//
+
+bool sendPacket(const uint8_t* buf, uint8_t bufSize) {
+  udp.beginPacket(broadcastAddress, TCPSlavePort);
+  udp.write(buf, bufSize);
+  return (udp.endPacket() == 1);
+}
+
+String receivePacket() {
+  char buffer[255];
+  buffer[0] = 0;
+
+  int packetSize = udp.parsePacket();
+  if (packetSize) {
+    int len = udp.read(buffer, 255);
+    if (len > 0) {
+      buffer[len] = 0;
+    }
+  }
+
+  if (strcmp(buffer, "Finding Bitlock") == 0) {
+    udp.beginPacket(udp.remoteIP(), udp.remotePort());
+    udp.write("You found me, lying on the floor");
+    udp.endPacket();
+    return "";
+  } else {
+    return String(buffer);
+  }
+}
+
+//
+//
+//   serverMaster page handling functions
+//
+//
+
+void handleClientRoot() {
+  if (serverMaster->method() != HTTP_POST) {
+    if (serverMaster->method() == HTTP_GET && master_unique != "") {
+      serverMaster->send(200, "text/plain", master_unique);
+    } else {
+      serverMaster->send(200, "text/plain", "ITS A ME, BITLOCK!");
+    }
+  }
+  else if (master_unique = "")
+  {
+    master_unique = serverMaster->arg("plain");
+
+    EEPROM.begin(256);
+
+    unsigned int i = 0;
+    master_unique = String("id=") + master_unique;
+    for (; i < master_unique.length(); i++) {
+      EEPROM.write(EEPROMaddress + i, master_unique[i]);
+      delay(200);
+    }
+
+    EEPROM.write(EEPROMaddress + i, '\0');
+    delay(200);
+
+    EEPROM.commit();
+
+    master_unique = master_unique.substring(4);
+
+    serverMaster->send(200, "text/plain", "OK!");
+  } else {
+    serverMaster->send(200, "text/plain", "Already registered, please stop");
+  }
+}
+
+void handleClientRegisterDevice() {
+  if (serverMaster->method() == HTTP_GET) {
+    serverMaster->send(200, "text/plain", waitingForRegister ? "Device waiting to be registered" : "No devices available for registering");
+
+  } else if (serverMaster->method() == HTTP_POST) {
+    String device_id = serverMaster->arg("plain");
+
+    if (waitingForRegister) {
+      WiFiClient client = slaveMap[""];
+      if(client.connected()) {
+        Serial.println("Connected to slave to be registered");
+        client.print("REGISTER AS ");
+        client.print(device_id);
+        client.print("\n");
+        client.flush();
+
+        if(client.available()) {
+          String response = client.readStringUntil('\n');
+          Serial.println("response: " + response);
+          if(response.equals("DONE")) {
+            Serial.println("response==DONE");
+            slaveMap[device_id] = client;
+            slaveMap.erase("");
+            waitingForRegister = false;
+            serverMaster->send(200, "text/plain", "Device sucessfully registered");
+          } else {
+            Serial.println("response!=DONE");
+            serverMaster->send(200, "text/plain", "Unable to register device");
+          }
+        }
+      } else {
+        Serial.println("Fail to connect to slave to be registered");
+      }
+
+    } else {
+      Serial.println("No devices available");
+      serverMaster->send(200, "text/plain", "No devices available for registering");
+    }
+  } else if (serverMaster->method() == HTTP_DELETE) {
+    String device_id = serverMaster->arg("plain");
+
+    if(slaveMap.find(device_id) != slaveMap.end()) {
+      WiFiClient client = slaveMap[device_id];
+      if(client.connected()) {
+        client.print("DELETE\n");
+        client.flush();
+        client.stop();
+
+        slaveMap.erase(device_id);
+      }
+    } else {
+      serverMaster->send(200, "text/plain", "No such device");
+    }
+  } else {
+    serverMaster->send(405, "text/plain", "Method not allowed");
+  }
+}
+
+//
+//
+//   Setup functions for the different running modes
+//
+//
+
+void setupMaster() {
+  Serial.println("1");
+  WiFi.begin(ssid, password);
+
+  Serial.println("2");
+  int result;
+  Serial.println("3");
+  int tries = 1;
+  Serial.println("4");
+  while ((result = WiFi.status()) != WL_CONNECTED && tries < 20) {
+    Serial.println("5");
+    tries++;
+    Serial.println("6");
+    delay(500);
+    Serial.println("7");
+  }
+  Serial.println("8");
+  if (result == WL_CONNECTED) {
+    Serial.println("9");
+    Serial.println("connected sucessfully to wifi");
+    if (gatewayOrMaster == 0) {
+      serverGateway.send(200, "text/plain", "Sucessfully connected");
+      serverGateway.stop();
+      WiFi.softAPdisconnect();
+    }
+
+    WiFi.softAPConfig(local_IP, gateway, subnet);
+    WiFi.softAP("BitLockMesh", "bitlockmesh", 1, 1);
+
+    Serial.println("does this happen");
+
+    serverMaster = new ESP8266WebServer(WiFi.localIP(), 80);
+    if (EEPROMaddress == 0) {
+      unsigned int i;
+      String temp = "wl=";
+      EEPROM.begin(256);
+      for (i = 0; i < 3; i++) {
+        EEPROM.write(EEPROMaddress + i, temp[i]);
+        delay(200);
+      }
+      EEPROMaddress += i;
+
+      for (i = 0; i < ssid.length(); i++) {
+        EEPROM.write(EEPROMaddress + i, ssid[i]);
+        delay(200);
+      }
+      EEPROM.write(EEPROMaddress + i, '\0');
+
+      EEPROMaddress += i + 1;
+      for (i = 0; i < password.length(); i++) {
+        EEPROM.write(EEPROMaddress + i, password[i]);
+        delay(200);
+      }
+      EEPROM.write(EEPROMaddress + i, '\0');
+      delay(200);
+
+      EEPROM.commit();
+
+      EEPROMaddress += i + 1;
+    }
+
+    broadcastAddress = (uint32_t)WiFi.softAPIP() | ~subnet;
+
+    serverMaster->on("/", handleClientRoot);
+    serverMaster->on("/devices/", handleClientRegisterDevice);
+    serverMaster->on("/reset/", handleReset);
+
+    serverMaster->begin();
+
+    //NEVER FORGET, EURO 2004
+    udp.begin(2004);
+    gatewayOrMaster = 1;
+  } else {
+    serverGateway.send(500, "text/plain", "Failed to connect");
+    gatewayOrMaster = 0;
+  }
+}
+
+void setupConnectGateway(void)
+{
+  WiFi.softAPConfig(local_IP, gateway, subnet);
+  WiFi.softAP("Bit_Lock");
+
+  serverGateway.on("/", handleServerRoot);
+
+  serverGateway.begin();
 }
--- slave_esp/slave_esp.ino
+++ slave_esp/slave_esp.ino
@@ -2,25 +2,31 @@
 #include <ESP8266WiFi.h>
 #include <EEPROM.h>
 
+const uint16_t UDPPort = 2016;
+const uint16_t TCPPort = 2019;
+
 WiFiUDP udp;
+WiFiServer server(TCPPort);
+WiFiClient master;
 
 char packetBuffer[255];
 String id;
 bool deleteMe;
+bool runServer;
 
 void setup() {
   Serial.begin(9600);
-  
+
   id = "";
   deleteMe = false;
+  runServer = false;
   WiFi.mode(WIFI_STA);
   WiFi.begin("BitLockMesh", "bitlockmesh");
 
-  while(WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED) {
     Serial.println(".");
     delay(1000);
   }
-
 
   EEPROM.begin(256);
 
@@ -41,53 +47,72 @@ void setup() {
 
 void loop() {
   delay(1000);
-  int packetSize = udp.parsePacket();
-  if (packetSize) {
-    IPAddress remoteIp = udp.remoteIP();
-
-    // read the packet into packetBufffer
-    int len = udp.read(packetBuffer, 255);
-    if (len > 0) {
-      packetBuffer[len] = 0;
-    }
-
-    String request = packetBuffer;
-    Serial.println(request);
-    String reply = "";
-    if (request.equals("REG_WAIT?")) {
-      if (id.equals("")) {
-        reply = "YES";
-      } else {
-        reply = "NO";
+  
+  if(runServer) {
+    if(master && master.connected()) {
+      String request;
+      if(master.available()) {
+        request = master.readStringUntil('\n');
       }
-    } else if (request.startsWith("REGISTER AS")) {
-      if (id.equals("")) {
-        id = request.substring(13);
-        EEPROM.begin(256);
-        String save = "id=" + id;
-        for (int i = 0; i < save.length(); i++) {
-          EEPROM.write(i, save[i]);
+
+      if(request.length() == 0) {
+
+      } else if (request.equals("REG_WAIT?")){
+        master.print(id.equals("") ? "YES\n" : id + "\n");
+        master.flush();
+      } else if (request.startsWith("REGISTER AS ")){
+        String requestId = request.substring(13);
+        if(id.equals("")) {
+          id = "id+" + requestId;
+          EEPROM.begin(256);
+          for(unsigned int i = 0; i < id.length(); i++) {
+            EEPROM.write(i, id[i]);
+          }
+          EEPROM.write(id.length(), '\0');
+          EEPROM.commit();
+
+          id = requestId;
+          master.print("DONE\n");
+        } else {
+          master.print("FAIL\n");
         }
-        EEPROM.write(save.length(), '\0');
-        EEPROM.commit();
-        reply = "DONE";
-      } else {
-        reply = "ALREADY REGISTERED";
-      }
-    } else if (request.startsWith("OPEN ")) {
-      if (request.substring(6).equals(id)) {
-        Serial.println("OPEN");
-      }
-    } else if (request.startsWith("DELETE ")) {
-      if(request.substring(8).equals(id)) {
+        master.flush();
+      } else if(request.equals("OPEN DOOR")) {
+        Serial.print("OPEN\n");
+      } else if(request.equals("DELETE")){
         deleteMe = true;
       }
+    } else {
+      master = server.available();
     }
+  } else {
+    int packetSize = udp.parsePacket();
+    if (packetSize) {
+      // read the packet into packetBufffer
+      int len = udp.read(packetBuffer, 255);
+      if (len > 0) {
+        packetBuffer[len] = 0;
+      }
 
-    // send a reply, to the IP EEPROMaddress and port that sent us the packet we received
-    udp.beginPacket(udp.remoteIP(), udp.remotePort());
-    udp.write(reply.c_str());
-    udp.endPacket();
+      String request = packetBuffer;
+      Serial.println(request);
+      String reply = "";
+      if (request.equals("Anyone there?")) {
+        reply = "Yes";
+        runServer = true;
+        server.begin();
+      }
+
+      delay(500);
+      // send a reply, to the IP and port that sent us the packet we received
+      if (!reply.equals("")) {
+        udp.beginPacket(udp.remoteIP(), udp.remotePort());
+        char buffer[255];
+        reply.toCharArray(buffer, 255);
+        udp.write(buffer);
+        udp.endPacket();
+      }
+    }
   }
 
   if (deleteMe) {
